1963de3c3064c8ea27bacdd1af792d587062dfaf
Kayla Ngan <kayla.578@gmail.com>
2016-01-29 23:08:17 -0500

    update graph options and colors

--- scripts/import_data_and_clean.R
+++ scripts/import_data_and_clean.R
@@ -22,7 +22,7 @@ all_states <- map_data("state")
 all_states_w_abbrev <- merge(all_states, state_to_abbrev, by="region")
 
 # pick a name and a year and a gender
-picked_name <- "Alia"
+picked_name <- "Sara"
 picked_year <- 1980
 picked_sex <- "F"
 selected_name_and_yr <- subset(dt, name==picked_name & year==picked_year & sex==picked_sex)
@@ -32,12 +32,13 @@ total <- merge(all_states_w_abbrev, selected_name_and_yr, by="state")
 # sort so plots without the lines in the map
 total <- total[order(total$order),]
 p <- ggplot()
-p <- p + geom_polygon(data=total, aes(x=long, y=lat, group = group, fill=total$count),colour="white"
-) + scale_fill_continuous(low = "thistle2", high = "darkred", guide="colorbar")
+p <- p + geom_polygon(data=total, aes(x=long, y=lat, group = group, fill=total$count),colour="Black"
+) + scale_fill_distiller(palette="Blues") +  guides(fill = guide_legend(reverse = TRUE))
 P1 <- p + theme_bw()  + labs(fill = "New Births" 
                              ,title = paste("Count of Name",picked_name,"(",picked_sex,") in",picked_year), x="", y="")
 P1 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border =  element_blank())
 
-# TODO: normalize?
+# TODO: normalize by births that year
 # TODO: pick "both" for gender
 # TODO: scales weird when some states are blank?
+# TODO: dark is high, light is low
